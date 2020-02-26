9fd67290f766be5d6c5c4476b6db8b33512c19f1
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2020-02-26 13:23:59 +0200

    add modal

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,37 @@
-'use strict';
+document.addEventListener('DOMContentLoaded', function (event) {
+    
+    'use strict';
+
+    // Модальное окно
+    const popup = () => {
+
+        const modal = document.querySelector('.modal'),
+            modalButton = document.querySelectorAll('[data-toggle=modal]'),
+            closeButton = document.querySelector('.modal__close');
+
+        const switchModal = () => {
+            modal.classList.toggle('modal--visible');
+        };
+
+        modalButton.forEach(elem => {
+            elem.addEventListener('click', switchModal);
+        });
+
+        closeButton.addEventListener('click', switchModal);
+
+        modal.addEventListener('click', (event) => {
+            let target = event.target;
+
+            if (target.closest('.modal')) {
+                target.classList.remove('modal--visible');
+            } else {
+                return;
+            }
+        });
+
+    };
+    popup();
+
+});
+
+
