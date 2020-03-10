68b2d0a1d029519faf17cc963e64acab63fe0f3e
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2020-03-10 17:08:10 +0200

    add send.php and phpmailer

---
+++ src/send.php
@@ -0,0 +1,46 @@
+<?php
+
+$userName = $_POST['user_name'];
+$userPhone = $_POST['user_phone'];
+$userEmail = $_POST['user_email'];
+$userQuestion = $_POST['user_question'];
+
+// Load Composer's autoloader
+require 'phpmailer/Exception.php';
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'semenovaleksandr407@gmail.com';                     // SMTP username
+    $mail->Password   = '05101992rko';                               // SMTP password
+    $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    //Recipients
+    $mail->setFrom('semenovaleksandr407@gmail.com', 'Mailer');
+    $mail->addAddress('sniper.semenov@ukr.net', 'Joe User');     // Add a recipient
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Новая заявка с сайта - "Турецкий ремонт в Ростове-на-Дону"';
+    $mail->Body    = '
+        Имя: ${userName}
+        Телефон: ${userPhone}
+        Email: ${userEmail}
+        Вопрос: ${userQuestion}
+    ';
+
+    $mail->send();
+    //echo 'Письмо отправлено';
+    header('Location: thanks.html');
+} catch (Exception $e) {
+    echo "Письмо не отправлено. Код ошибки: {$mail->ErrorInfo}";
+}
